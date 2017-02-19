39daae0142f0428bf1f1637dc0f0b08c6b9f3554
Matthew Nathan Blanchard <matthew.n.blanchard@maine.edu>
2017-02-19 17:05:25 -0500

    Modified morse.c to open GPIO pins 4 & 17

--- morse.c
+++ morse.c
@@ -6,20 +6,38 @@
  * standard output.
  */
 
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "encoding.h"
 
+#define HIGH 1
+#define LOW 0
+
+int GPIO_open(int pin);                      // Open pin 'pin' for output
+int GPIO_close(int pin);                     // Close pin 'pin'
+int GPIO_write(int pin, int val);            // Write "value" to pin
 void p_encode(char c, unsigned char *sum);   // Translates a character to the appropriate morse string and prints it. Tracks checksum
 
 int main(int argc, char *argv[]) {
         
-        int i = 0;                       // String index 
+        int i = 0;                        // String index 
         unsigned char checksum = 0;       // Checksum of 'high' Morse time units
 
         // Expecting one argument (which will be interpreted as a string)
         if (argc != 2) {
                 printf("Error: a single string as an argument\n");
                 return 1;
+        }
+
+        // Set up GPIO pins
+        if (GPIO_open(4)) {
+                printf("Fatal Error: Could not open GPIO pin 4 ...\n");
+                return 2;
+        }
+        if (GPIO_open(17)) {
+                printf("Fatal Error: Could not open GPIO pin 17 ...\n");
+                return 2;
         }
         
         // Copy string argument
@@ -49,6 +67,81 @@ int main(int argc, char *argv[]) {
         printf("\n");
         return 0;
 }
+
+int GPIO_open(int pin)
+{
+        char buf[3];            // Max two digits in pin string, plus null termintator
+        ssize_t size;           // Size of 'pin' string
+        int file;               // File descriptor
+        char dir_path[35];      // File path for direction
+
+        // Open GPIO pin
+        file = open("/sys/class/gpio/export", O_WRONLY);  // Open GPIO export
+        if (file == -1) {
+                printf("Failed to open GPIO export\n");
+                return 1;
+        }
+
+        size = snprintf(buf, 3, "%d", pin);   // Write GPIO pin to open to file
+        write(file, buf, size);               
+        close(file);                          // Close export file
+
+        // Set GPIO pin direction
+        snprintf(dir_path, 35, "/sys/class/gpio/gpio%d/direction", pin);
+        file = open(dir_path, O_WRONLY);
+        if (file == -1) {
+                printf("Failed to open GPIO direction\n");
+                return 2;
+        }
+        
+        if (!(write(file, "out", 3))) {
+                printf("Failed to write direction to GPIO\n");
+                return 3;
+        }       
+        close(file);
+
+        return 0;
+}
+
+int GPIO_close(int pin)
+{
+        char buf[3];   // Max two digits in pin string, plus null terminator
+        ssize_t size;  // Size of 'pin' string
+        int file;      // File descriptor
+
+        file = open("/sys/class/gpio/unexport", O_WRONLY);  // Open GPIO unexport
+        if (file == -1) {
+                printf("Failed to open GPIO unexport\n");
+                return 1;
+        }
+
+        size = snprintf(buf, 3, "%d", pin); // Write GPIO pin to close to file
+        write(file, buf, size);
+        close(file);
+        return 0;      
+}
+
+int GPIO_write(int pin, int value)
+{
+        int file;                // File descriptor
+        char write_path[30];     // File path for value    
+        char input[2] = "01";    // Possible inputs
+
+        snprintf(write_path, 30, "/sys/class/gpio/gpio%d/value", pin);
+        file = open(write_path, O_WRONLY);
+        if (file == -1) {
+                printf("Failed to open GPIO value\n");
+                return 1;
+        }
+        
+        if (1 != write(file, value == 0 ? &input[0] : &input[1], 1)) {
+                printf("Failed to write to GPIO\n");
+                return 2;
+        }
+        close(file);
+        return 0;
+}
+
 void p_encode(char c, unsigned char *sum) 
 {
         unsigned int binary = 0b0;        // Binary Morse translation of char
